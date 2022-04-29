b28427f51a97c2cd1177cc383ffb72753e42a76a
Scott the Builder <scottsaunders@mac.com>
2022-04-29 16:45:18 -0400

    added correct block pattern functionality.

--- functions.php
+++ functions.php
@@ -1,31 +1,62 @@
-<?php 
+<?php
 
-	if ( ! function_exists( 'block_theme_setup' ) ) :
-		function block_theme_setup() {
-			// Add support for block styles.
-			add_theme_support( 'wp-block-styles' );
-			// Enqueue editor styles.
-			add_editor_style( 'editor-style.css' );
-			// Enqueue Google Fonts to Appear both in the Admin Area and Front End. Google hosted fonts are not fully supported.
-			wp_enqueue_style( 'google_fonts', '//fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap',true,'1.1','all');
-		}
-		add_action( 'after_setup_theme', 'block_theme_setup' );
-	
-	endif;
-	
-	
-	/* Enqueue scripts and styles */
-	if ( ! function_exists( 'theme_enqueue_scripts_styles' ) ) :
-	
-		function theme_enqueue_scripts_styles() {
-		    wp_enqueue_style( 'mytheme-style', get_template_directory_uri() . '/assets/css/main_min.css',false,'1.1','all');
-		    wp_enqueue_style( 'material_fonts', '//fonts.googleapis.com/css?family=Material+Icons',true,'1.1','all');
-			wp_enqueue_script( 'scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0', true );
-		}
-		add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts_styles' );
-	
-	endif;
-	
-	// Remove Core Block Patterns
-	remove_theme_support( 'core-block-patterns' );
-	include_once 'patterns/pattern_controller.php';
+if (!function_exists("block_theme_setup")):
+  function block_theme_setup()
+  {
+    // Add support for block styles.
+    add_theme_support("wp-block-styles");
+    // Enqueue editor styles.
+    add_editor_style("editor-style.css");
+    // Enqueue Google Fonts to Appear both in the Admin Area and Front End. Google hosted fonts are not fully supported.
+    wp_enqueue_style(
+      "google_fonts",
+      "//fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Poppins:wght@300;400;500&family=Roboto+Slab:wght@300;400&display=swap",
+      true,
+      "1.1",
+      "all"
+    );
+  }
+  add_action("after_setup_theme", "block_theme_setup");
+endif;
+
+/* Enqueue scripts and styles */
+if (!function_exists("theme_enqueue_scripts_styles")):
+  function theme_enqueue_scripts_styles()
+  {
+    wp_enqueue_style(
+      "mytheme-style",
+      get_template_directory_uri() . "/assets/css/main_min.css",
+      false,
+      "1.1",
+      "all"
+    );
+    wp_enqueue_style(
+      "material_fonts",
+      "//fonts.googleapis.com/css?family=Material+Icons",
+      true,
+      "1.1",
+      "all"
+    );
+    wp_enqueue_script(
+      "scripts",
+      get_template_directory_uri() . "/assets/js/scripts.js",
+      ["jquery"],
+      "1.0",
+      true
+    );
+  }
+  add_action("wp_enqueue_scripts", "theme_enqueue_scripts_styles");
+endif;
+
+// Remove Core Block Patterns
+remove_theme_support("core-block-patterns");
+
+register_block_pattern_category("hero", [
+  "label" => __("Hero"),
+]);
+
+register_block_pattern_category("column_layout", [
+  "label" => __("Column Layout"),
+]);
+
+include_once "patterns/pattern_controller.php";
--- patterns/block_pattern_one.php
+++ patterns/block_pattern_one.php
@@ -1,12 +1,9 @@
 <?php
-
-register_block_pattern(
-	'layouts/three_column_layout',
-	array(
-		'title' => __( 'Three Column Layout', 'textdomain' ),
-		'categories' => array( 'acfcore', 'contact-form' ),
-		'keywords'   => array( 'columns' ),
-		'content'   => '<!-- wp:columns {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px"}}}} -->
+register_block_pattern("layouts/three_column_layout", [
+  "title" => __("Three Column Layout", "textdomain"),
+  "categories" => ["column_layout"],
+  "keywords" => ["columns"],
+  "content" => '<!-- wp:columns {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px"}}}} -->
 <div class="wp-block-columns" style="padding-top:20px;padding-bottom:20px"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:paragraph -->
 <p>Column One</p>
@@ -25,5 +22,4 @@ register_block_pattern(
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->',
-	)
-);
+]);
