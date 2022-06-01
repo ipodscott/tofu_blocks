dddaeeb565a5b7a4d68ebc1352cc2bc92e8bf9b7
ipodscott <scottsaunders@mac.com>
2022-06-01 00:34:31 -0400

    added new title blocks

---
+++ patterns/big-title copy.php
@@ -0,0 +1,12 @@
+<?php
+/**
+  * Title: Left Movie Title
+  * Slug: titles/left_movie_title
+  * Categories: titles, text
+  */
+?>
+
+
+<!-- wp:paragraph {"align":"center","className":"left-movie-title"} -->
+<p class="has-text-align-center left-movie-title">Left Movie Title</p>
+<!-- /wp:paragraph -->
